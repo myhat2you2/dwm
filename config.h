626e472c68319f598f4d0882728596fdcef46ce4
myhat2you2 <joshuadtalbot3@gmail.com>
2022-08-31 19:05:29 +0100

    font size update

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx	    = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "Font Awesome 6 Free Solid:size=12" };
+static const char *fonts[]          = { "monospace:size=12", "Font Awesome 6 Free Solid:size=10" };
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -61,21 +61,18 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-#include "patches/shiftview.c"
 static const Key keys[] = {
 	/* modifier                     key		function        argument */
 	{ MODKEY,			XK_Return,	spawn,		{.v = termcmd } },
 	{ MODKEY,			XK_d,		spawn,		{.v = dmenucmd} },
 	{ MODKEY,             		XK_q,      	killclient,    	{0} },
+	{ MODKEY,                       XK_b,      	togglebar,      {0} },
 	{ MODKEY,                       XK_Tab,    	view,           {0} },
 	{ MODKEY|ShiftMask,		XK_Return,	zoom,		{0} },
-	{ MODKEY|ShiftMask,		XK_b,      	togglebar,      {0} },
 	{ MODKEY,                       XK_j,      	focusstack,    	{.i = +1 } },
 	{ MODKEY,                       XK_k,      	focusstack,    	{.i = -1 } },
 	{ MODKEY,                       XK_i,      	incnmaster,    	{.i = +1 } },
 	{ MODKEY,                       XK_o,      	incnmaster,    	{.i = -1 } },
-	{ MODKEY,			XK_b,	 	shiftview,	{.i = -1 } },
-	{ MODKEY,			XK_n,	 	shiftview,	{.i = +1 } },
 	{ MODKEY,                       XK_h,      	setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      	setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_t,      	setlayout,      {.v = &layouts[0]} },
