ce5582844555e9f62c79ba2ec2cb262a4db0b646
myhat2you2 <joshuadtalbot3@gmail.com>
2022-10-09 12:12:15 +0100

    fullscreen patch

--- config.h
+++ config.h
@@ -51,7 +51,7 @@ static const Rule rules[] = {
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -90,7 +90,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_h,      	setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      	setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_t,      	setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      	setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      	                togglefullscreen, {0} },
 	{ MODKEY,                       XK_space,  	setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  	togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_0,      	tag,            {.ui = ~0 } }, //???
