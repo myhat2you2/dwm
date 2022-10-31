a41ab7ce2397205e39a57c2abf59bf36e250e2c8
myhat2you2 <joshuadtalbot3@gmail.com>
2022-10-31 14:23:45 +0000

    added perstack and movestack patches

--- config.h
+++ config.h
@@ -75,6 +75,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key		function        argument */
 	{ MODKEY,			XK_Return,	spawn,		{.v = termcmd } },
@@ -85,8 +86,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_Return,	zoom,		{0} },
 	{ MODKEY,                       XK_j,      	focusstack,    	{.i = +1 } },
 	{ MODKEY,                       XK_k,      	focusstack,    	{.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,      	incnmaster,    	{.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      	incnmaster,    	{.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      	movestack,    	{.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      	movestack,    	{.i = -1 } },
 	{ MODKEY,                       XK_h,      	setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      	setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_t,      	setlayout,      {.v = &layouts[0]} },
