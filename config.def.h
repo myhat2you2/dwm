0473b1aa972d799676fd73ee482314a563a5366d
myhat2you2 <joshuadtalbot3@gmail.com>
2022-11-21 19:02:34 +0000

    minor changes

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_blue[]        = "#406590";
+static const char col_blue[]        = "#294470";
 static const char col_cyan[]        = "#1a98b8";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
